0e76a7b09dd51a4f28c9d762fc29be62e95c6197
Kurt <ksphotopro@gmail.com>
2017-05-23 21:49:15 -0400

    comments added

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 "use strict";
 
+//returns the NodeList w/ the passed class name
 const gamePieces = returnCardElements('game-piece');
 
+//returns a randomly selected batch of icon unicodes
 const selectedIcons = returnRandomIcons(allCodes, gamePieces.length/2);
 
+//duplicates the randim icons, concats the arrays to create pairs and shuffles the whole deck
 const shuffledIconPairs = pairAndShuffle(selectedIcons);
 
-console.log(shuffledIconPairs);
+//console.log(shuffledIconPairs);
 
-
+//sets the inner HTML of each gamePiece to an icon based on the shuffled deck
 printInnerHTML(gamePieces, shuffledIconPairs)
 
 
--- gameFunctions.js
+++ gameFunctions.js
@@ -1,14 +1,12 @@
 "use strict";
 
+//returns the NodeList w/ the passed class name
 const returnCardElements = target => document.getElementsByClassName(target);
 
+//generates a random number between 0 and a passed upper limit (num)
 const returnRandomNum = num => Math.floor(Math.random()*num);
 
-const pairAndShuffle = function(arr){
-    let pair =  arr.concat(arr);
-    return pair.sort(function(a, b){return 0.5 - Math.random()});
-}
-
+//picks randomly from an input array (arr) to create and return an array with a length = outputLength
 const returnRandomIcons = function(arr, outputLength){
     let outputArray = [];
 
@@ -23,6 +21,14 @@ const returnRandomIcons = function(arr, outputLength){
     return pushToOutput(arr, outputLength, outputArray);
 }
 
+//concats an array (arr) and shuffles the deck
+const pairAndShuffle = function(arr){
+    let pair =  arr.concat(arr);
+    return pair.sort(function(a, b){return 0.5 - Math.random()});
+}
+
+//the content of each element of a nodelist (elements) is filled in sequence by it's equivalent index of the contentArr array. 
+//default start=0 and ends the recursion at elements.length
  const printInnerHTML = (elements, contentArr, start = 0) => {
      if (start === elements.length){
          return null;
