533bb9971bb5f137ab42cb2978fe809fe5ee9f90
Kurt <ksphotopro@gmail.com>
2017-05-23 21:32:28 -0400

    cleaned frankenfunction

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
 "use strict";
 
-
-
-
-
-
-
-//console.log(shuffledIconPairs);
-
-
 printInnerHTML(returnCardElements('game-piece'), pairAndShuffle(returnRandomIcons(allCodes, returnCardElements('game-piece').length/2)))
 
 
